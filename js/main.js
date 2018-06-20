ed4e6a5ba25b290b5d19b11505b9de024698be1a
Renata Andrade <renataandrade1987@hotmail.com>
2018-06-20 12:27:02 -0300

    atualização do projeto

--- js/main.js
+++ js/main.js
@@ -1,61 +1,151 @@
-let perguntas = [
-    "quiz.html",
-    "pergunta2.html",
-    "pergunta3.html",
-    "pergunta4.html",
-    "pergunta5.html",
-    "pergunta6.html",
-    "pergunta7.html",
-    "pergunta8.html",
-    "pergunta9.html",
-    "pergunta10.html",
-    "pergunta11.html",
-    "pergunta12.html",
-    "pergunta13.html",
-];
-let gabarito = [
-    'sim', 
-    'sim', 
-    'sim', 
-    'sim', 
-    'nao', 
-    'nao', 
-    'sim', 
-    'sim', 
-    'nao', 
-    'sim'
+let quiz = [
+    {
+    pergunta: 'Zomba ou constrange você na frente dos amigos e da família?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Minimiza suas conquistas ou não incentiva você a conquistar seus sonhos?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Faz com que você se sinta incapaz de tomar decisões?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Usa da intimidação, culpa e ameaças para obter sua complacência?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Diz o que você deve ou não vestir?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Fala como deve deixar ou pentear o cabelo?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Diz que você não é nada sem ele ou que ele não é nada sem você?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Te trata de maneira grosseira sem o seu consentimento, beliscando, agarrando, empurrando ou até agredindo?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Faz várias ligações por noite ou aparece para garantir que você está onde disse que estaria?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Usa as drogas e álcool como desculpa para dizer coisas indelicadas ou para abusar de você?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Te culpa pela maneira com que age ou se sente?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Dá a sensação de que “não há como sair” do relacionamento?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    },
+    {
+    pergunta: 'Evita que você faça outras coisas que gosta, como ficar perto da família e de amigos?',
+    imagem: 'img/images_012.png',
+    alt: 'descrição da imagem',
+    respostaSim: 'sim',
+    respostaNão: 'não',
+    }
 ]
-let respostas = new Array();
-let resultado = new Array()
+
+let respostaSim = []
+let respostaNão = []
 let i = 0
 let acertos = 0
+let erros = 0
+
+document.querySelector('.quiz').innerHTML = `
+    <img src=${quiz[i].imagem} alt=${quiz[i].alt} />
+    <p>${quiz[i].pergunta}</p>
+    <div class="btn">
+        <button id="btn-sim" onclick="onClick(this.value)" value="não">NÃO</button>
+        <button id="btn-nao" onclick="onClick(this.value)" value="sim">SIM</button>
+    </div>
+`
 
 function onClick(valor){
-    respostas [i] = valor;
+    respostaSim[i] = valor
+    respostaNão[i] = valor
+   
+    if(respostaSim[i] === quiz[i].respostaNão) {
+        erros++
+    } else {
+        (respostaNão[i] === quiz[i].respostaSim)
+        acertos++
 
-    if(respostas[i] == gabarito[i]){
-        resultado[i] = "Mana chuta que é a macumba, relacionamente existe pra te fazer feliz se o boy e desse jeito cai fora que o mundo é grande"
-        acertos++;
-    }else {
-        resultado[i] = "Mana parabéns seu relacionamento é como tem que ser baseado no respeito"
     }
-    i++
-    if(i < perguntas.length){
-        // document.getElementById("#btn").html = perguntas[i]
-        window.location.href = perguntas[i];
+
+    if (i < quiz.length) {
+        console.log(quiz[i])
+        document.querySelector('.quiz').innerHTML = `
+            <img src=${quiz[i].imagem} alt=${quiz[i].alt} />
+            <p>${quiz[i].pergunta}</p>
+            <div class="btn">
+            <button id="btn-sim" onclick="onClick(this.value)" value="não">NÃO</button>
+            <button id="btn-nao" onclick="onClick(this.value)" value="sim">SIM</button>
+        </div>
+        `
+        i++
+    } else {
+        
+        let mensagem = ''
+    
+        if (respostaSim > respostaNão){
+         
+            mensagem = 'Mana parabéns seu relacionamento é como tem que ser baseado no respeito onde um respeita o espaço do outro'
+            
+        } else {
+            mensagem = 'Mana chuta que é a laço, relacionamente existe pra te fazer feliz se o boy é desse jeito, cai fora que o mundo é grande'
+        }
+        document.querySelector('.quiz').innerHTML = `
+        <p>${mensagem}</p>
+        <img src=${quiz[i].imagem} alt=${quiz[i].alt} />
+        `
+        
     }
+
 }
-// let perguntas = 0
-// let respostas = 0 
-// var respostaFinal = new Array ();
-
-// function btn(){
-//     window.location.replace(pergunta2.html);
-//     if (document.getElementById("btn-sim").value === "sim") {    
-//         respostaFinal[0]="certo";
-//            perguntas++;
-//        }else{
-//         document.getElementById("btn-nao").value === "nao";
-//         respostaFinal[0]="errado";
-//         respostas++;}
-// }
